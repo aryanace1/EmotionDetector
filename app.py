4088b42b996e36eab504dc0e5cc97b0a595391c2
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-04-15 23:08:05 +0530

    Add comments

--- app.py
+++ app.py
@@ -27,17 +27,26 @@ def index():
 
 @app.route('/snapImage', methods=['POST'])
 def snapImage():
+
+    # Get the image data from the request
     image_data = request.form.get('imageData')
+
+    # Decode the image data
     image_data = base64.b64decode(image_data.split(',')[1])
     img = PIL.Image.open(io.BytesIO(image_data)).convert('L')
+
+    # Resize the image and convert it to an array
     img = img.resize((48, 48))
     img_array = img_to_array(img)
     img_array = img_array.reshape(
         (1, img_array.shape[0], img_array.shape[1], 1))
+
     prediction = model.predict(img_array)
     predicted_class = np.argmax(prediction)
     predicted_emotion = class_names[predicted_class]
+
     response = {'prediction': predicted_emotion}
+
     return jsonify(response)
 
 

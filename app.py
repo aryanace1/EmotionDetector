c0e8c66a5140594c88770be16188fc2b3309408a
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-04-15 20:59:02 +0530

    Add snap feature

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
 
-    return render_template('index.html')
+        # Retrieve the image file from the POST request
+        f = request.files['image']
+
+        # Save the image file to disk
+        filename = 'snapshot.jpg'
+        f.save(filename)
+
+        # Process the image file as desired
+
+        return render_template("index.html")
+
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,5 @@
 let video = document.getElementById("videoElement");
+let canvas = document.createElement("canvas");
 let stream = null;
 
 function startWebcam() {
@@ -21,4 +22,28 @@ function stopWebcam() {
     stream.getTracks()[0].stop();
     stream = null;
   }
+}
+
+function takeSnapshot() {
+  if (stream != null) {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext("2d").drawImage(video, 0, 0);
+    canvas.toBlob(sendSnapshot, "image/jpeg");
+  }
+}
+
+function sendSnapshot(blob) {
+  let formData = new FormData();
+  formData.append("image", blob, "snapshot.jpg");
+  fetch("/", {
+    method: "POST",
+    body: formData,
+  })
+    .then(function (response) {
+      console.log("Image sent to server.");
+    })
+    .catch(function (error) {
+      console.error("Error sending image to server: " + error);
+    });
 }
